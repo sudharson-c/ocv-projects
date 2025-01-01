21df8e3f9369b8519ecc6513ae59326b268145ca
sudharson-c <sudharson@student.tce.edu>
2025-01-01 12:57:39 +0530

    update

---
+++ traffic-sign/main.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+from PIL import Image
+import os
+os.chdir('./myData')
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+
+data = []
+labels = []
+# We have 43 Classes
+classes = 43
+cur_path = os.getcwd()
+
+for i in range(classes):
+    path = os.path.join(cur_path,str(i))
+    images = os.listdir(path)
+    for a in images:
+        try:
+            image = Image.open(path + '/'+ a)
+            image = image.resize((30,30))
+            image = np.array(image)
+            data.append(image)
+            labels.append(i)
+        except Exception as e:
+            print(e)
+
+data = np.array(data)
+labels = np.array(labels)
+
+print(data.shape, labels.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=0)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+y_train = to_categorical(y_train, 43)
+y_test = to_categorical(y_test, 43)
+
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(rate=0.5))
+# We have 43 classes that's why we have defined 43 in the dense
+model.add(Dense(43, activation='softmax'))
+
+#Compilation of the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+epochs = 20
+history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
+
+# accuracy
+plt.figure(0)
+plt.plot(history.history['accuracy'], label='training accuracy')
+plt.plot(history.history['val_accuracy'], label='val accuracy')
+plt.title('Accuracy')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
+
+# Loss
+plt.plot(history.history['loss'], label='training loss')
+plt.plot(history.history['val_loss'], label='val loss')
+plt.title('Loss')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+test_score = model.evaluate(X_test, y_test, verbose=0)
+print(f"Test Loss: {test_score[0]}")
+print(f"Test Accuracy: {test_score[1]}")
+
+# Saving the Model
+model.save("traffic_sign_model.h5")
+print("Model saved as traffic_sign_model.h5")
